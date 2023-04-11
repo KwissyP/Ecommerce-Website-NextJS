f0a791e6f1800714a7e87d2720d4b8a69cb860ea
KwissyP <christianpandoulas@hotmail.com>
2023-04-11 09:48:41 +0200

    Essai mongodb

---
+++ src/utils/data.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+    users: [
+        {
+            name: 'John',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('password'),
+            idAdmin: true,
+        },
+        {
+            name: 'Jane',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('password'),
+            idAdmin: false,
+        },
+    ],
+}
+
+export default data;
