250f2cdae6e0947bd215d8d2b6d737de1744c1e5
KwissyP <christianpandoulas@hotmail.com>
2023-04-14 10:50:25 +0200

    Légère rectification carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ products }) {
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
 
-  const addToCartHandler =async(product) => {
+  const addToCartHandler = async (product) => {
     const existItem = cart.cartItems.find((x) => x.id === product.id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${product._id}`)
@@ -25,23 +25,23 @@ export default function Home({ products }) {
       return;
     }
 
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity }});
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
     toast.success('Product added to the cart successfully');
   };
 
   return (
     <Layout title="Home Page">
-      <Carousel showThumbs={false} autoPlay>
-  <Link href="/product/9" className='flex'>
-    <img src="./assets/nike.png" alt="Nike" className="w-96 h-96 object-contain" />
-  </Link>
-  <Link href="/product/16" className='flex'>
-    <img src="./assets/converse.png" alt="Converse" className="w-96 h-96 object-contain" />
-  </Link>
-  <Link href="/product/18" className='flex'>
-    <img src="./assets/goyard.png" alt="Goyard" className="w-96 h-96 object-contain" />
-  </Link>
-</Carousel>
+      <Carousel showThumbs={false} autoPlay infiniteLoop>
+        <Link href="/product/9" className='flex'>
+          <img src="./assets/nike.png" alt="Nike" className="w-96 h-96 object-contain" />
+        </Link>
+        <Link href="/product/16" className='flex'>
+          <img src="./assets/converse.png" alt="Converse" className="w-96 h-96 object-contain" />
+        </Link>
+        <Link href="/product/18" className='flex'>
+          <img src="./assets/goyard.png" alt="Goyard" className="w-96 h-96 object-contain" />
+        </Link>
+      </Carousel>
       <h2 className="text-lg my-4 font-bold">Latest Products :</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
@@ -56,7 +56,7 @@ export async function getServerSideProps() {
   await db.connect();
   const products = await Product.find().lean();
   return {
-    props:{
+    props: {
       products: products.map(db.convertDocToObj)
     },
   }
